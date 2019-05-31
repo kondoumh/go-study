3a9d4d0b6c9fc40262289b720fac3b3741bfe190
kondoumh <kondoumh@gmail.com>
2019-05-31 12:25:15 +0900

    add interface example

--- atourofgo/methods-and-dependency/interfaces/interfaces.go
+++ atourofgo/methods-and-dependency/interfaces/interfaces.go
@@ -13,6 +13,12 @@ func main() {
 	var a Abser
 	f := MyFloat(-math.Sqrt2)
 	v := Vertex{3, 4}
+
+	a = f  // a MyFloat implements Abser
+	a = &v // a *Vertex implements Abser
+	
+	//a = v // v is a Vertex (not implement Abser)
+	fmt.Println(a.Abs())
 }
 
 type MyFloat float64
