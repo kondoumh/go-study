8370a7219ed0247dd378245128853db2f982be25
kondoumh <kondoumh@gmail.com>
2019-07-21 11:22:06 +0900

    add example of testable example

--- minago/testing/calc/calc_test.go
+++ minago/testing/calc/calc_test.go
@@ -1,9 +1,17 @@
 package calc
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+)
 
 func TestSum(t *testing.T) {
 	if sum(1, 2) != 3 {
 		t.Fatal("sum(1,2) shuoud be 3. but doesn't match")
 	}
+}
+
+func ExampleHello() {
+	fmt.Println(sum(1, 2))
+	// Output: 3
 }
