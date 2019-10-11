8b68960890c2e9dfcdc913f504695ccbf61d8517
kondoumh <kondoumh@gmail.com>
2019-10-11 12:47:11 +0900

    Replace NewCallback to FuncOf

--- gowasm/dom-event/main.go
+++ gowasm/dom-event/main.go
@@ -7,8 +7,9 @@ func main() {
 
 	message := window.Get("document").Call("getElementById", "message")
 
-	cb := js.NewCallback(func(args []js.Value) {
+	cb := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		message.Set("innerHTML", "Clicked!!")
+		return nil
 	})
 
 	message.Call("addEventListener", "click", cb)
