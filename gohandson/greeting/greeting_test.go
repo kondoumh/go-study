5e0b4cbba77f1d139c7f9328178bca6a50e6c317
kondoumh <kondoumh@gmail.com>
2019-10-28 12:59:44 +0900

    Devide test package

--- gohandson/greeting/greeting_test.go
+++ gohandson/greeting/greeting_test.go
@@ -1,28 +1,28 @@
-package greeting
+package greeting_test
 
 import (
 	"bytes"
 	"testing"
 	"time"
+
+	"github.com/tenntenn/gohandson/greeting"
 	"golang.org/x/text/language"
 )
 
 func TestGreeting_Do(t *testing.T) {
-	orgLang := lang
-	lang = language.Japanese
-	defer func() {
-		lang = orgLang
-	}()
+	defer greeting.ExportSetLang(language.Japanese)()
 
-	g := Greeting{
-		Clock: ClockFunc(func() time.Time {
+	g := greeting.Greeting{
+		Clock: greeting.ClockFunc(func() time.Time {
 			return time.Date(2018, 8, 31, 06, 0, 0, 0, time.Local)
 		}),
 	}
+
 	var buf bytes.Buffer
 	if err := g.Do(&buf); err != nil {
 		t.Error("unexpected error", err)
 	}
+
 	if expected, actual := "おはよう", buf.String(); expected != actual {
 		t.Errorf("greeting message wont %s but got %s", expected, actual)
 	}
