d28002993acea57f74afcd87dca48d2a82345e23
kondoumh <kondoumh@gmail.com>
2019-10-31 00:06:12 +0900

    Add goroutine handson folder

---
+++ gohandson/goroutine/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type (
+	Bean       int
+	GroundBean int
+	Water      int
+	HotWater   int
+	Coffee     int
+)
+
+const (
+	GramBeans            Bean       = 1
+	GramGroundBeans      GroundBean = 1
+)
