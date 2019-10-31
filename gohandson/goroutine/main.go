4b3f885bc9cebe910490071c0df806cc62379311
kondoumh <kondoumh@gmail.com>
2019-11-01 05:31:28 +0900

    WIP: Add example

--- gohandson/goroutine/main.go
+++ gohandson/goroutine/main.go
@@ -16,4 +16,19 @@ type (
 const (
 	GramBeans            Bean       = 1
 	GramGroundBeans      GroundBean = 1
+	MilliLitterWater     Water      = 1
+	MilliLiterHotWater   HotWater   = 1
+	CupsCofee            Coffee     = 1
 )
+
+func (w Water) String() string {
+	return fmt.Sprintf("%d[ml] water", int(w))
+}
+
+func (hw HotWater) String() string {
+	return fmt.Sprintf("%d[ml] hot water", int(hw))
+}
+
+func (b Bean) String() string {
+	return fmt.Sprintf("%d[g] beans", int(b))
+}
