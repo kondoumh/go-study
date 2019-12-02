cd9e19adc2ce2e13af34e8aee5d78252760746f0
kondoumh <kondoumh@gmail.com>
2019-12-02 12:51:24 +0900

    Move fetch function

---
+++ cli/scrapbox/fetch_pages.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func FetchPages(projectName string, limit *int, order *string, skip *int) {
+	url := fmt.Sprintf("https://scrapbox.io/api/pages/%s?skip=%d&limit=%d&sort=%s", projectName, *skip, *limit, *order)
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("[status] %d\n", res.StatusCode)
+	for k, v := range res.Header {
+		fmt.Println("[header] " + k)
+		fmt.Println(": " + strings.Join(v, ","))
+	}
+
+	file, err := os.Create("out.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var pj bytes.Buffer
+	json.Indent(&pj, []byte(body), "", " ")
+	file.Write(pj.Bytes())
+}
--- cli/scrapbox/pages.go
+++ cli/scrapbox/pages.go
@@ -1,15 +1,7 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"flag"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
-	"strings"
 )
 
 func main() {
@@ -17,33 +9,5 @@ func main() {
 	order := flag.String("o", "updated", "ordering key {updated|views|linked}")
 	skip := flag.Int("s", 0, "skip count")
 	flag.Parse()
-	get(limit, order, skip)
-}
-
-func get(limit *int, order *string, skip *int) {
-	url := fmt.Sprintf("https://scrapbox.io/api/pages/kondoumh?skip=%d&limit=%d&sort=%s", *skip, *limit, *order)
-	res, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("[status] %d\n", res.StatusCode)
-	for k, v := range res.Header {
-		fmt.Println("[header] " + k)
-		fmt.Println(": " + strings.Join(v, ","))
-	}
-
-	file, err := os.Create("out.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-	defer res.Body.Close()
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var pj bytes.Buffer
-	json.Indent(&pj, []byte(body), "", " ")
-	file.Write(pj.Bytes())
+	FetchPages("kondoumh", limit, order, skip)
 }
