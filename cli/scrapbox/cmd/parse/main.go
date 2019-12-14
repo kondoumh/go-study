1cb1470db6ede0e8c0b5a05c45c0463cc57d4800
kondoumh <kondoumh@gmail.com>
2019-12-15 01:17:06 +0900

    Add loop for parse details

--- cli/scrapbox/cmd/parse/main.go
+++ cli/scrapbox/cmd/parse/main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 )
+
+type Project struct {
+	Name  string `json:"projectName"`
+	Count int    `json:"count"`
+	Skip  int    `json:"skip"`
+	Pages []Page `json:"pages"`
+}
 
 type Page struct {
 	Id            string `json:"id"`
@@ -23,19 +31,34 @@ type User struct {
 }
 
 func main() {
-	bytes, err := ioutil.ReadFile("_out/page_detail.json")
+	projectName := flag.String("p", "kondoumh", "project name")
+	pjdata, err := ioutil.ReadFile("_out/" + *projectName + ".json")
 	if err != nil {
 		log.Fatal(err)
 	}
-	var page Page
-	if err := json.Unmarshal(bytes, &page); err != nil {
+	var project Project
+	if err := json.Unmarshal(pjdata, &project); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%s : %s\n", page.Id, page.Title)
-	fmt.Printf("%d %d\n", page.Views, page.Linked)
-	fmt.Printf("Author : %s %s %s\n", page.Author.Id, page.Author.Name, page.Author.DisplayName)
-	fmt.Printf("Collaborators:\n")
-	for _, user := range page.Collaborators {
-		fmt.Printf("%s %s %s\n", user.Id, user.Name, user.DisplayName)
+
+	fmt.Printf("%d\n", project.Count)
+
+	for i := 0; i < project.Count-1; i++ {
+		sfx := fmt.Sprintf("-%d.json", i+1)
+		bytes, err := ioutil.ReadFile("_out/" + *projectName + sfx)
+		if err != nil {
+			log.Fatal(err)
+		}
+		var page Page
+		if err := json.Unmarshal(bytes, &page); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s : %s\n", page.Id, page.Title)
+		fmt.Printf("%d %d\n", page.Views, page.Linked)
+		fmt.Printf("Author : %s %s %s\n", page.Author.Id, page.Author.Name, page.Author.DisplayName)
+		fmt.Printf("Collaborators:\n")
+		for _, user := range page.Collaborators {
+			fmt.Printf("%s %s %s\n", user.Id, user.Name, user.DisplayName)
+		}
 	}
 }
