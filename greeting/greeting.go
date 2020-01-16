500b40af9e3bce698faf3800ae116ea9629cfde4
kondoumh <kondoumh@gmail.com>
2020-01-16 15:02:12 +0900

    Add func

--- greeting/greeting.go
+++ greeting/greeting.go
@@ -1,6 +1,8 @@
 package greeting
 
 import (
+	"errors"
+
 	"rsc.io/quote"
 	quoteV3 "rsc.io/quote/v3"
 )
@@ -11,4 +13,16 @@ func Greet() string {
 
 func Proverb() string {
 	return quoteV3.Concurrency()
+}
+
+func DoMinus(arg int) int {
+	res, _ := minus9(arg)
+	return res
+}
+
+func minus9(num int) (int, error) {
+	if num < 10 {
+		return 0, errors.New("num shoud be 10 or more!")
+	}
+	return num - 9, nil
 }
--- greeting/greeting_test.go
+++ greeting/greeting_test.go
@@ -15,3 +15,10 @@ func TestProverb(t *testing.T) {
 		t.Errorf("Proverb() = %q, want %q", got, want)
 	}
 }
+
+func TestDoMinus(t *testing.T) {
+	want := 91
+	if got := DoMinus(100); got != want {
+		t.Errorf("DoMinus(100) = %d, want %d", got, want)
+	}
+}
