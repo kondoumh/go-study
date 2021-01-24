da9af0d267fd447b94fd2d607406a43b63052e88
kondoumh <kondoumh@gmail.com>
2021-01-24 09:18:25 +0900

    Add example to make elements of slice unique using map

---
+++ sd202101/1/unique.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	followers := []string{"John", "Richard", "John", "Jane", "Jane", "Alan"}
+	unique := make([]string, 0, len(followers))
+	m := make(map[string]struct{})
+	for _, v := range followers {
+		if _, ok := m[v]; ok {
+			continue
+		}
+		unique = append(unique, v)
+		m[v] = struct{}{}
+	}
+	fmt.Println(unique)
+}
