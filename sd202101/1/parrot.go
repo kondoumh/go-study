3afbafbb4e9aa1ce55ae66dd6f0c4a99b74c495c
kondoumh <kondoumh@gmail.com>
2021-01-25 22:50:37 +0900

    Add example of impl of interface as anonymous function

---
+++ sd202101/1/parrot.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type Crier interface {
+	Cry() string
+}
+
+type ParrotFunc func() string
+
+func (p ParrotFunc) Cry() string {
+	return p()
+}
+
+func main() {
+	var c Crier = ParrotFunc(func() string {
+		return "Squawk"
+	})
+	fmt.Println(c.Cry())
+}
