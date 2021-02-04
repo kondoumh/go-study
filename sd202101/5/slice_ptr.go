aa01209a74ba0ed34ef4789cc978af68773ea0a4
kondoumh <kondoumh@gmail.com>
2021-02-04 12:47:04 +0900

    Add slice append example

---
+++ sd202101/5/slice_ptr.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	type T struct {
+		Number int
+	}
+
+	s := []*T{{1}, {2}, {3}, {4}, {5}}
+	s2 := []*T{}
+	for _, v := range s {
+		s2 = append(s2, v)
+	}
+	for _, v := range s2 {
+		fmt.Printf("%+v\n", v)
+	}
+}
