dcad303aee251bea101ce6db80e9040c3ddd38dc
kondoumh <kondoumh@gmail.com>
2021-02-05 00:03:13 +0900

    Add struct prt example

---
+++ sd202101/5/struct_ptr.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type T struct {
+	Number int
+	Text string
+}
+
+func main() {
+	type Container struct { V *T }
+	c := Container{V: &T{}}
+	v := c.V
+	v.Number = 1
+	fmt.Println(c.V.Number)
+	c.V.Text = "hello"
+	fmt.Println(c.V.Text)
+}
