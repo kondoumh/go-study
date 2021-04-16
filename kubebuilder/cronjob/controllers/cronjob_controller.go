ec72c7fd8bca92dded751e807cd424fa08d37f2a
kondoumh <kondoumh@gmail.com>
2021-04-16 19:32:28 +0900

    WIP implementing a controller

--- kubebuilder/cronjob/controllers/cronjob_controller.go
+++ kubebuilder/cronjob/controllers/cronjob_controller.go
@@ -40,7 +40,7 @@ func (r *CronJobReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	ctx = context.Background()
 	log = r.Log.WithValues("cronjob", req.NamespacedName)
 
-	// your logic here
+	// 1:Load the CronJob by name
 	var cronJob batch.CronJob
 	if err := r.Get(ctx, req.NamespacedName, &cronJob); err != nil {
 		log.Error(err, "unable to fetch CronJob")
@@ -48,6 +48,100 @@ func (r *CronJobReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 		// requeue (we'll need to wait for a new notification), and we can get them
 		// on deleted requests.
 		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	// 2:List all active jobs, and update the status
+	var childJobs kbatch.JobList
+	if err := r.List(ctx, &childJobs, client.InNamespace(req.Namespace), client.MatchingField{jobOwnerKey: req.Name}); err != nil {
+		log.Error(err, "unable to list child Jobs")
+		return ctrl.Result{}, err
+	}
+	// find the active list of jobs
+	var activeJobs []*kbatch.Job
+	var successfullJobs []*kbatch.Job
+	var failedJobs []*kbatch.Job
+	var mostRecentTime *time.Time // find the last run so we can update the status
+
+	isJobFinished := func(job *kbatch.Job) (bool, kbatch.JobConditionType) {
+		for _, c := range job.Status.Conditions {
+			if (c.Type == kbatch.JobComplete || c.Type == kbatch.JobFailed) && c.Status == corev1.ConditionTrue {
+				return true, c.Type
+			}
+		}
+		return false, ""
+	}
+
+	getScheduledTimeForJob := func(job *kbatch.Job) (*time.Time, error) {
+		timeRaw := job.Annotations[scheduledTimeAnnotation]
+		if len(timeRaw) == 0 {
+			return nil, nil
+		}
+
+		timeParsed, err := time.Parse(time.RFC3339, timeRaw)
+		if err != nil {
+			return nil, err
+		}
+		return &timeParsed, nil
+	}
+
+	for i, job := range childJobs.Items {
+		_, finishedType := isJobFinished(&job)
+		switch finishedType {
+		case "": // ongoing
+			activeJobs = append(activeJobs, &childJobs.Items[i])
+		case kbatch.JobFailed:
+			failedJobs = append(failedJobs, &childJobs.Items[i])
+		case kbatch.JobComplete:
+			successfullJobs = append(successfullJobs, &childJobs.Items[i])
+		}
+
+		// We'll store the launch time in an annotation, so we'll reconstitute that from
+		// the active jobs themselves.
+		scheduledTimeForJob, err := getScheduledTimeForJob(&job)
+		if err != nil {
+			log.Error(err, "unable to parse schedule time for child jobb", "job", &job)
+			continue
+		}
+		if scheduledTimeForJob ! = nil {
+			if mostRecentTime == nil {
+				mostRecentTime = scheduledTimeForJob
+			} else if mostRecentTime.Before(*scheduledTimeForJob) {
+				mostRecentTime = scheduledTimeForJob
+			}
+		}
+
+		if mostRecentTime != nil {
+			cronJob.Status.LastScheduleTime = &metav1.Time{Time: *mostRecentTime}
+		} else {
+			cronJob.Status.LastScheduleTime = nil
+		}
+		cronJob.Status.Active = nil
+		for _, activeJob := range activeJobs {
+			jobRef, err := ref.GetReference(r.Scheme, activeJob)
+			if err != nil {
+				log.Error(err, "unable to make reference to active job", "job", activeJob)
+				continue
+			}
+			cronJob.Status.Active = append(cronJob.Status.Active, *jobRef)
+		}
+
+		// 3: Clean up old jobs according to the history limit
+		if cronJob.Spec.FailedJobsHistoryLimit != nil {
+			sort.Slice(failedJobs, func(i, j int) bool{
+				if failedJobs[i].Status.StartTime != nil {
+					return failedJobs[j].Status.StartTime != nil
+				}
+				return failedJobs[i].Status.StartTime.Before(failedJobs[j].Status.StartTime)
+			})
+		}
+
+	}
+
+	log.V(1).Info("job count", "active Jobs", len(activeJobs), "successful jobs", len(successfullJobs), "failed jobs", len(failedJobs))
+
+	if err := r.Status().Update(ctx, &cronJob); err != nil {
+		log.Error(err, "unable to update CronJob status")
+		return ctrl.Result{}, err
 	}
 
 	return ctrl.Result{}, nil
